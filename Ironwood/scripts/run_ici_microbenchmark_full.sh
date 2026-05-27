688ae563ad53b02a19156bb43f89982b2c3b96ae
VidushiVashishth <vvashishth@google.com>
2026-05-27 11:50:33 -0700

    Perf testing of ici collectives

--- Ironwood/scripts/run_ici_microbenchmark_full.sh
+++ Ironwood/scripts/run_ici_microbenchmark_full.sh
@@ -7,14 +7,14 @@
 
 # CONFIG_NAMES='reduce_scatter_1d reduce_scatter_2d all_gather_3d all_reduce_3d all_to_all_3d all_gather_2d all_reduce_2d all_to_all_2d all_gather_1d all_reduce_1d all_to_all_1d'
 
-# CONFIG_NAMES='all_gather_1d all_reduce_1d all_to_all_1d reduce_scatter_1d all_gather_3d all_reduce_3d all_to_all_3d all_gather_2d all_reduce_2d all_to_all_2d reduce_scatter_2d' 
+ CONFIG_NAMES='all_gather_1d all_reduce_1d all_to_all_1d reduce_scatter_1d all_gather_3d all_reduce_3d all_to_all_3d all_gather_2d all_reduce_2d all_to_all_2d reduce_scatter_2d' 
 
-CONFIG_NAMES='all_to_all_1d reduce_scatter_1d all_reduce_1d' 
+# CONFIG_NAMES='all_to_all_1d reduce_scatter_1d all_reduce_1d' 
 
 for CONFIG in $CONFIG_NAMES
 do
   # Construct the full config file path
-  CONFIG_FILE="Ironwood/configs/collectives/8x8x16/${CONFIG}.yaml"
+  CONFIG_FILE="Ironwood/configs/collectives/4x4x4/${CONFIG}.yaml"
   
   echo "--- Starting benchmark for ${CONFIG} ---"
   
